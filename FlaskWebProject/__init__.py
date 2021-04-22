4ac44f004f6e997d1212483c9f6f9e559a3a8a57
Abdulhadi Ahmad <get2abdul101@yahoo.com>
2021-04-22 12:46:53 +0100

    import error fixed

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -5,7 +5,6 @@ The flask application package.
 #from FlaskWebProject import views
 
 
-import FlaskWebProject.views
 import logging
 from flask import Flask
 from config import Config
@@ -25,3 +24,5 @@ Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+
+import FlaskWebProject.views
